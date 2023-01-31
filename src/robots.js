c5bdfe517eee7049943d49a78cbed9393be09a1f
allek1992 <115907603+allek1992@users.noreply.github.com>
2023-01-31 20:11:01 +0100

    temp commit

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Aleksandar Joksimovic',
+        username: 'Alek',
+        email: 'allek1992@hotmail.com'
+    },
+    {
+        id: 2,
+        name: 'Nemanja Ivankovic',
+        username: 'Gasa',
+        email: 'gasa1992@hotmail.com'
+    },
+    {
+        id: 3,
+        name: 'Milos Mitic',
+        username: 'Mitke',
+        email: 'mitke1992@hotmail.com'
+    },
+    {
+        id: 4,
+        name: 'Ilija Veselinovic',
+        username: 'Deda',
+        email: 'deda1992@hotmail.com'
+    },
+    {
+        id: 5,
+        name: 'Ljubisa Rancic',
+        username: 'Bika',
+        email: 'bika1993@hotmail.com'
+    },
+    {
+        id: 6,
+        name: 'Lazar Pantic',
+        username: 'Laza',
+        email: 'laza1992@hotmail.com'
+    },
+    {
+        id: 7,
+        name: 'Vlada Todorovic',
+        username: 'vlajko',
+        email: 'vlajko1992@hotmail.com'
+    },
+    {
+        id: 8,
+        name: 'Zaharije Jovanovic',
+        username: 'Zare',
+        email: 'zare1992@hotmail.com'
+    },
+    {
+        id: 9,
+        name: 'Nikola Tmusic',
+        username: 'Muva',
+        email: 'muva1993@hotmail.com'
+    },
+    {
+        id: 10,
+        name: 'Marko Stankovic',
+        username: 'Mare',
+        email: 'mare1992@hotmail.com'
+    }
+]
